8471a9bb667a13d7d352fb3fb4cc67758cabdedd
Henry Hite <hohite@uh.edu>
2021-02-04 00:42:07 -0600

    First Commit
    Commit should be just creating the visualization for AR

---
+++ Hello World!/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Speed of object
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHoriz, 0.0f, moveVert);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+}
---
+++ Hello World!/Assets/Scripts/Token.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Token : MonoBehaviour
+{
+
+    // Scoring
+    public int points = 1;
+    public GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Player>())
+        {
+            Debug.Log("*coin noises*");
+            this.gameObject.SetActive(false);
+            gm.ObtainPoints(points);
+
+        }
+    }
+}
